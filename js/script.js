f212f4571c63bc060dcb5d76fb54a0c01b3c15c9
Samuel Tunes <samueltunes55@gmail.com>
2024-11-13 21:30:20 -0300

    Carregando um modelo 3d .glb

--- js/script.js
+++ js/script.js
@@ -1,108 +1,61 @@
-main();
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-function NormalisedToDevice(coord, axisSize){
-  var halfAxisSize = axisSize / 2;
+import { DDSLoader} from 'three/addons/loaders/DDSLoader.js';
+import { MTLLoader} from 'three/addons/loaders/MTLLoader.js';
+import { OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 
-  var deviceCoord = ( coord + 1 ) *halfAxisSize;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, 640.0 / 480.0, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
+camera.position.z = 5;
 
-  return deviceCoord;
+var object, texture, objectLoader = false;
+
+function onProgress(xhr) {
+} 
+
+function onError() {
 }
 
-function DeviceToNormalised(coord, axisSize){
-  var halfAxisSize = axisSize / 2;
+var manager = new THREE.LoadingManager
+manager.addHandler( /\.dds$/i, new DDSLoader() );
 
-  var normaliseCoord = ( coord /halfAxisSize ) -1;
+var light = new THREE.HemisphereLight(0xffffff, 0x000000);
+light.position.set(0, 50, 0);
+scene.add(light);
 
+new MTLLoader(manager)
+    .setPath('../model/').load('Cordeiro1.mtl', function (materials){
+        materials.preload();
 
-  return normaliseCoord;
-}
+        new OBJLoader(manager).setMaterials(materials).setPath('../model').load('/Cordeiro1.obj', function(obj){
+            object = obj;
 
+            object.scale.x =0.03;
+            object.scale.y =0.03;
+            object.scale.z =0.03;
 
-function main(){
-  const canvas = document.querySelector("#glcanvas");
-  const gl = canvas.getContext ("webgl");
+            object.position.y = -1;
 
-  if( !gl ){
-    alert("Unable to setup WEBGL. Your browser or computer may not support it;")
-    return;
+            scene.add(object);
+            objectLoader=true;
+        },onProgress, onError);
+    });
+function animate() {
+  requestAnimationFrame(animate);
+
+  if(objectLoader){
+  object.rotation.x +=0.002;
+  object.rotation.y +=0.002;
+  object.rotation.z +=0.002;
   }
 
-  var vertices = [
-  -0.5, 0.5 , 0.0, //1
-  -0.5, -0.5, 0.0, //2
-  0.5, -0.5, 0.0, //3
-  0.5, 0.5, 0.0 // 4
-  ];
-
- var indices = [ 3, 2, 1, 3, 1, 0 ];
-  var vertex_buffer = gl.createBuffer();
- 
-  gl.bindBuffer( gl.ARRAY_BUFFER, vertex_buffer);
-
-  gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-   var Index_Buffer = gl.createBuffer();
-  
-   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
-
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-  var vertCode= 
-    'attribute vec3 coordinates;' + 
-    'void main(void)' +
-    '{' +
-      'gl_Position= vec4(coordinates, 1.0);'+
-      'gl_PointSize=10.0;' +
-    '}'
-
-  var vertShade = gl.createShader( gl.VERTEX_SHADER);
-
-  gl.shaderSource(vertShade, vertCode);
-
-  gl.compileShader(vertShade);
-
-  var fragCode=
-    'void main(void)' +
-    '{' +
-        'gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);'+
-      '}';
-  
-  var fragShade = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragShade, fragCode);
-
-  gl.compileShader(fragShade);
-
-  var shaderProgram = gl.createProgram();
-
-  gl.attachShader(shaderProgram, vertShade);
-  gl.attachShader(shaderProgram, fragShade);
-
-  gl.linkProgram(shaderProgram);
-  
-  gl.useProgram(shaderProgram);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
-
-  var coord = gl.getAttribLocation(shaderProgram, "coordinates");
-
-  gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0 ,0);
-
-  gl.enableVertexAttribArray(coord);
-
-  gl.enable(gl.DEPTH_TEST);
-
-  gl.clearColor(1.0, 0.0, 0.0, 0.0 );
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  gl.viewport(0, 0, canvas.width, canvas.height);
-
-   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-
+  renderer.render(scene, camera);
 }
+
+animate();
